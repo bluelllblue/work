3fdacd3d6e19834c9f0c621ec8ea6d76703a2e31
bluelllblue <2179192627@qq.com>
2023-06-12 18:15:21 +0800

    test1

--- src/views/personal/index.vue
+++ src/views/personal/index.vue
@@ -1,23 +1,15 @@
 <template>
-    <div>
-        个人中心
-    </div>
+  <div></div>
 </template>
 <script>
-
-export default{
-    name:'Personal', //组件名称
-    data(){
-        return{
-
-        }
-    },
-    methods:{
-
-    },
-}
+export default {
+  name: "Personal", //组件名称
+  data() {
+    return {};
+  },
+  methods: {},
+};
 </script>
 <!-- scoped: 作用域，当前css只当前的组件生效-->
 <style lang="less" scoped>
-
 </style>
